22a7d1439a62ad2b6afb7e143e4160e89a431179
hancoma <hancoma@gmail.com>
2017-08-09 05:47:26 +0900

    수정오료

--- www/js/index.js
+++ www/js/index.js
@@ -88,7 +88,7 @@ push.on('registration', function(data) {
 });
 
 push.on('notification', function(data) {
-  alert(data.message);
+//  alert(data.message);
  // display_call_info(data.message);
   alert_msg("알람",data.message);
  
